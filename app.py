8b07ca626ee2f5955120aacb1195f7885489231a
Renan Santana <renan_ota@hotmail.com>
2025-07-04 18:35:00 -0300

    Create type and convert values to int

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 import sqlite3
 from datetime import datetime
+from typing import TypedDict
 
 import requests
 from dotenv import load_dotenv
 
 
 class Database:
+    """
+    TODO: Adapt for postgres.
+    """
     load_dotenv()
     connection = sqlite3.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
@@ -28,6 +32,11 @@ class Database:
         self.connection.commit()
 
 
+class DollarData(TypedDict):
+    timestamp: int
+    dollar_value: int
+
+
 class API:
     load_dotenv()
     api_key = os.getenv("API_KEY")
@@ -35,22 +44,27 @@ class API:
     target_value = "bid"  # or "ask"?
 
     @classmethod
-    def get_value(cls) -> dict:
+    def get_data(cls) -> DollarData:
+        """
+        Load data from awesomeapi.
+        Dollar value will be rounded, and transformed in a integer.
+        """
         response = requests.get(f"{cls.url}?token={cls.api_key}")
         if response.status_code != 200:
             # TODO: Report error
             return
         data = response.json()
-        timestamp = data["USDBRL"]["timestamp"]
+        timestamp = int(data["USDBRL"]["timestamp"])
         dollar_value_str = data["USDBRL"][cls.target_value]
-        dollar_value = round(float(dollar_value_str), 2)
-        return {"timestamp": timestamp, "dollar_value": dollar_value}
+        dollar_value = str(round(float(dollar_value_str), 2))
+        dollar_value_int = int(dollar_value.replace(".", ""))
+        return {"timestamp": timestamp, "dollar_value": dollar_value_int}
 
 # Test area -------------------------------------------------------------------
 test = API()
 database = Database()
 
-data = test.get_value()
+data = test.get_data()
 print(data)
 
 datetime = datetime.fromtimestamp(int(data["timestamp"]))
