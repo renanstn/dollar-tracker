a95c2b272f3b344d8a8efce216514db72e98d5d0
renan <renan_ota@hotmail.com>
2025-07-10 19:41:52 -0300

    Add CORS to api

--- api/app.py
+++ api/app.py
@@ -3,10 +3,12 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app, origins=["https://dollar-tracker.up.railway.app"])
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(DATABASE_URL)
