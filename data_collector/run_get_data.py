c500dcaee0b68aa920b7accdb9b5fe544ef74940
Renan Santana <renan_ota@hotmail.com>
2025-07-19 14:20:16 -0300

    Avoid duplicate key errors

--- data_collector/run_get_data.py
+++ data_collector/run_get_data.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytz
 from app import API
 from database import Database
+from psycopg2 import errors
 
 collector = API()
 database = Database()
@@ -11,4 +12,8 @@ data = collector.get_data()
 tz = pytz.timezone("America/Sao_Paulo")
 datetime_utc = datetime.fromtimestamp(int(data["timestamp"]))
 datetime_with_timezone = pytz.utc.localize(datetime_utc).astimezone(tz)
-database.load_values(datetime_with_timezone, data["dollar_value"])
+try:
+	database.load_values(datetime_with_timezone, data["dollar_value"])
+except errors.UniqueViolation:
+	database.connection.rollback()
+	exit()
