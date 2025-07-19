f20c698e8a9f6b43282d7ffa8eb7ef7b1e607130
Renan Santana <renan_ota@hotmail.com>
2025-07-19 14:24:43 -0300

    Format code with black

--- data_collector/run_get_data.py
+++ data_collector/run_get_data.py
@@ -13,7 +13,7 @@ tz = pytz.timezone("America/Sao_Paulo")
 datetime_utc = datetime.fromtimestamp(int(data["timestamp"]))
 datetime_with_timezone = pytz.utc.localize(datetime_utc).astimezone(tz)
 try:
-	database.load_values(datetime_with_timezone, data["dollar_value"])
+    database.load_values(datetime_with_timezone, data["dollar_value"])
 except errors.UniqueViolation:
-	database.connection.rollback()
-	exit()
+    database.connection.rollback()
+    exit()
